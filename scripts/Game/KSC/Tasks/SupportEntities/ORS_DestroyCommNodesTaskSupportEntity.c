29b8d3723b575bd52fc27cd42c362c62be7fd3eb
Kex <kexanone@gmail.com>
2026-08-18 21:53:28 +0200

    Remove task support entities

--- scripts/Game/KSC/Tasks/SupportEntities/ORS_DestroyCommNodesTaskSupportEntity.c
+++
@@ -1,9 +0,0 @@
-//------------------------------------------------------------------------------------------------
-class ORS_DestroyCommNodesTaskSupportEntityClass : KSC_CounterTaskSupportEntityClass
-{
-}
-
-//------------------------------------------------------------------------------------------------
-class ORS_DestroyCommNodesTaskSupportEntity : KSC_CounterTaskSupportEntity
-{
-}
